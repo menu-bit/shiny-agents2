cb7a5d61e2c13de7b3be5e9ea8bd976ea51fe865
menaka <shakya.menaka@gmail.com>
2025-09-04 11:53:41 +0200

    Scopez votre CSS avec styled components

---
+++ src/components/Header/Header/index.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: #5843E4;`}
+`
+
+function Header() {
+  return (
+    <nav>
+      <StyledLink to="/" $isFullLink>
+        Accueil
+      </StyledLink>
+      <StyledLink to="/survey/1" $isFullLink>
+        Questionnaire
+      </StyledLink>
+      <StyledLink to="/freelances" $isFullLink>
+        Profils
+      </StyledLink>
+    </nav>
+  )
+}
+
+export default Header
